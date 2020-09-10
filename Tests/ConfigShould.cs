3cd4ab80e82a0238370d47d75f8b06217523ec59
Grégory ALDEBERT <gregory.aldebert@storm-soft.com>
2020-09-10 15:19:30 +0200

    Ajout d'événement sur option sélectionnée

--- Tests/ConfigShould.cs
+++ Tests/ConfigShould.cs
@@ -10,24 +10,70 @@ namespace DDDTraining.Tests
     public class ConfigShould
     {
         [Fact]
-        public async Task Raise_Option_A_Selected()
+        public async Task Raise_Model_A_Event_Selected()
         {
             var eventStore = new EventStoreStub();
             var config = new Config(eventStore);
-            await config.SelectModelA();
+            await config.SelectModel1();
 
             Assert.Contains(eventStore.GetEvents(), e =>e is ModelSelectedEvent modelSelectedEvent &&
-                                                        modelSelectedEvent.Model == "A");
+                                                        modelSelectedEvent.Model.Id == "1");
+        }
+
+        [Fact(DisplayName ="Quand je choisis le model 1, j'ai bien les options A et B, et si ont choisit A on a un event sur A choisi")]
+        public async Task Raise_Option_A_when_A_And_B_Available_And_A_Selected()
+        {
+            var eventStore = new EventStoreStub();
+            var config = new Config(eventStore);
+            await config.SelectModel1();
+            Assert.Contains(eventStore.GetEvents(), e => e is OptionAvailableEvent optionAvailableEvent &&
+                                                         optionAvailableEvent.Options.Any(option => option.Id == "A") &&
+                                                         optionAvailableEvent.Options.Any(option => option.Id == "B"));
+            Assert.Contains(eventStore.GetEvents(), e => e is OptionSelectedEvent optionSelectedEvent &&
+                                                            optionSelectedEvent.Option.Id == "A" && 
+                                                            optionSelectedEvent.Model.Id == "1");
+
         }
     }
 
     public class ModelSelectedEvent : Event
     {
-        public string Model { get; }
+        public Model Model { get; }
 
-        public ModelSelectedEvent(string model)
+        public ModelSelectedEvent(Model model)
         {
             Model = model;
+        }
+
+    }
+    public class OptionAvailableEvent : Event
+    {
+
+        public IEnumerable<Option> Options { get; }
+        public OptionAvailableEvent(IEnumerable<Option> options)
+        {
+            Options = options;
+        }
+    }
+
+    public struct Option
+    {
+        public string Id { get; }
+        public Option(string id)
+        {
+            Id = id;
+        }
+    }
+
+    public class OptionSelectedEvent : Event
+    {
+        public Model Model { get; }
+        public Option Option { get; }
+
+        public OptionSelectedEvent(Model model, Option option)
+        {
+            Model = model;
+            Option = option;
         }
 
     }
@@ -35,14 +81,32 @@ namespace DDDTraining.Tests
     public sealed class Config
     {
         private readonly IEventStore store;
+        private readonly Model model1 = new Model("1");
+
         public Config(IEventStore store)
         {
             this.store = store;
         }
 
-        public Task SelectModelA()
-            => store.Publish(new ModelSelectedEvent("A"));
+        public async Task<Model> SelectModel1()
+        {
+            await store.Publish(new ModelSelectedEvent(model1));
+            await store.Publish(new OptionAvailableEvent(new[] { new Option("A"), new Option("B") }));
+            await store.Publish(new OptionSelectedEvent(model1, new Option("A")));
+            return model1;
+        }
     }
+
+    public struct Model
+    {
+        public string Id { get; }
+        
+        public Model(string id)
+        {
+            Id = id;
+        }
+    }
+
     public interface IEventStore
     {
         Task Publish<TEvent>(TEvent @event) where TEvent : Event;
