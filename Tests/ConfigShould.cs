b062df71de7c12d0fcd920876e7e5f7fe38b3b1a
Grégory ALDEBERT <gregory.aldebert@storm-soft.com>
2020-09-11 10:21:41 +0200

    Ajout d'une projection de liste de configurations

--- Tests/ConfigShould.cs
+++ Tests/ConfigShould.cs
@@ -77,12 +77,50 @@ namespace DDDTraining.Tests
         }
     }
 
-    //public class ConfigListProjectionShould
-    //{
-    //    [Fact]
-    //    public void 
-    //}
-    
+    public class ConfigListProjectionShould
+    {
+        private static readonly UserProfileId UserProfileId1 = new UserProfileId(Guid.NewGuid());
+        private static readonly Model model1 = new Model("1");
+        private IEnumerable<Event> InitializeEventHistory(UserProfileId userProfileId)
+        {
+            var model1 = new Model("1");
+            var optionA = new Option("A");
+            return new Event[]
+                {
+                new ModelSelectedEvent(userProfileId, model1),
+                new OptionAvailableEvent(userProfileId, new []{ optionA, new Option("B")}),
+                new OptionSelectedEvent(userProfileId, model1, optionA)
+                };
+        }
+        [Fact]
+        public void When_A_Model_Is_Selected_Projection_Is_Updated()
+        {
+            //var events = InitializeEventHistory(UserProfileId1);
+            var projection = new ConfigListProjection();
+            projection.Apply(new ModelSelectedEvent(UserProfileId1, model1));
+
+            var foundModel = projection.GetModelsByUserProfiles().FirstOrDefault(model => model.Key.Equals(UserProfileId1)).Value;
+
+            Assert.Equal(model1, foundModel);
+        }
+    }
+
+    public class ConfigListProjection
+    {
+        private readonly Dictionary<UserProfileId,Model> selectedModels = new Dictionary<UserProfileId, Model>();
+
+        public void Apply(Event @event)
+        {
+            switch(@event)
+            {
+                case ModelSelectedEvent modelSelectedEvent:
+                    selectedModels[modelSelectedEvent.UserId] = modelSelectedEvent.Model;
+                    break;
+            }
+        }
+
+        public IDictionary<UserProfileId, Model> GetModelsByUserProfiles() => selectedModels;
+    }
 
     public class ModelSelectedEvent : Event
     {
